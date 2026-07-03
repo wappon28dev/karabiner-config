36ef7d6b6b701ac282e4231a0d6d5d5e4a9729e3
wappon28dev <wappon_28_dev@ruru.be>
2026-07-03 11:08:22 +0900

    feat: change trigger of raycast (Option + Space)

--- src/rules/global.ts
+++ src/rules/global.ts
@@ -3,43 +3,35 @@ import { swapKeys, swapArrows, pcStyleHomeEnd } from "./utils.js";
 
 export function globalRules() {
   return [
-    // 1. Swap colon/semicolon
     swapKeys("semicolon", "left_shift", "Swap colon/semicolon"),
 
-    // 2. Swap quote/double_quote
     swapKeys("quote", "left_shift", "Swap quote/double_quote"),
 
-    // 3. Swap plus/equal
     swapKeys("equal_sign", "left_shift", "Swap plus/equal"),
 
-    // 4. Cmd + Shift + Q to Cmd + W
     rule("Cmd + Shift + Q to Cmd + W").manipulators([
       map("q", "⌘⇧").to("w", "⌘"),
     ]),
 
-    // 5. Cmd + Shift + Q to Cmd + Shift + W in VSCode
     rule(
       "Cmd + Shift + Q to Cmd + Shift + W in VSCode",
       ifApp("^com\\.microsoft\\.VSCode$"),
     ).manipulators([map("q", "⌘⇧").to("w", "⌘⇧")]),
 
-    // 6. Cmd (+ Shift) + Tab -> Ctrl (+ Shift) + Tab
     rule("Cmd (+ Shift) + Tab -> Ctrl (+ Shift) + Tab").manipulators([
       map("tab", "⌘⇧").to("tab", "⌃⇧"),
       map("tab", "⌘").to("tab", "⌃"),
     ]),
 
-    // 7. Single Cmd to Cmd + Space
-    rule("Single Cmd to Cmd + Space").manipulators([
+    rule("Single Cmd to Option + Space").manipulators([
       map("left_command")
         .to("left_command")
         .toIfAlone([
-          toKey("spacebar", "left_command"),
+          toKey("spacebar", "left_option"),
           toKey("japanese_eisuu", undefined, { lazy: true }),
         ]),
     ]),
 
-    // 8. Swap Cmd + Backspace and Option + Backspace
     rule("Cmd + Backspace to Option + Backspace").manipulators([
       map("delete_or_backspace", "⌘").to("delete_or_backspace", "⌥"),
     ]),
@@ -47,14 +39,11 @@ export function globalRules() {
       map("delete_or_backspace", "⌥").to("delete_or_backspace", "⌘"),
     ]),
 
-    // 9. Arrow keys command/option swaps
     ...swapArrows("left"),
     ...swapArrows("right"),
 
-    // 10. PC-Style Home/End Keys
     pcStyleHomeEnd(),
 
-    // 11. Ctrl + Backspace to Cmd + Backspace
     rule("Ctrl + Backspace to Cmd + Backspace").manipulators([
       map("delete_or_backspace", "⌃").to("delete_or_backspace", "⌘"),
     ]),
